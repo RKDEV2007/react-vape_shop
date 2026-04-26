a56c955c12e38cd0151f7baae60b46b6899516d8
Ob1ch <romakarpov90@gmail.com>
2026-04-26 17:09:50 +0300

    splitted products to dif file

--- vape_shop/src/App.jsx
+++ vape_shop/src/App.jsx
@@ -1,14 +1,13 @@
 import './App.css'
 import BorderGlow from './BorderGlow';
+import { products } from './Poducts';
 function App() {
   const navItems = ['Каталог', 'Одноразки', 'Жидкости', 'Аксессуары', 'Контакты'];
-  const products = [
-    { category: 'Одноразка', name: 'WAKA SoPro PA10000', details: 'Вкус: Berry Ice • 20 мг', price: '1 390 ₽', image: '/assets/waka-sopro-pa10000.jpg' },
-    { category: 'Одноразка', name: 'WAKA Smash 6000', details: 'Вкус: Mango Peach • 20 мг', price: '1 090 ₽', image: '/assets/waka-smash-6000.jpg' },
-    { category: 'Pod-система', name: 'Smoant Pasito 3', details: 'Мощность: до 50W • Картридж 3 мл', price: '3 990 ₽', image: '/assets/smoant-pasito-3.jpg' },
-    { category: 'Жидкость', name: 'Chaser Salt Grape Soda 30 мл', details: 'Крепость: 20 мг • Соотношение 50/50', price: '790 ₽', image: '/assets/chaser-grape-soda-30ml.jpg' },
-    { category: 'Аксессуар', name: 'Smoant хлопок (веревка)', details: 'Для намотки и обслуживания', price: '350 ₽', image: '/assets/smoant-cotton-rope.jpg' },
-  ];
+
+//фильтрация продуктов
+// потом сделать страницу основную, потом много страниц для каждого продукта
+//галлерея для каждого продукта
+//подвал
 
   return (
     <div className="app">
